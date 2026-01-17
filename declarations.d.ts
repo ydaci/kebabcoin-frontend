1b41a89e6de35488d2ec47d620c18d782e904fb3
ydaci <ydaci59@gmail.com>
2026-01-17 19:29:26 +0100

    Fix add Radix + Lucide module declarations for Vercel build

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,3 +1,7 @@
 // declarations.d.ts
 declare module '@radix-ui/react-accordion';
+declare module '@radix-ui/react-alert-dialog';
+declare module '@radix-ui/react-dialog';
+declare module '@radix-ui/react-popover';
+declare module '@radix-ui/react-tooltip';
 declare module 'lucide-react';
