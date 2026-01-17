bed2300725623584009718089fd2bbaa24ff2af6
ydaci <ydaci59@gmail.com>
2026-01-17 15:43:32 +0100

    Maj library

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  webpack: (config) => {
+    // Ignore le module React Native AsyncStorage
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      '@react-native-async-storage/async-storage': false,
+    }
+    return config
+  },
+}
 
 module.exports = nextConfig
